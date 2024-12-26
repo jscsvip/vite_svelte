cbfcd89fea3bedf2b99de19a8a0d39953bc0dc0e
李龙 <lilong2576@hanweb.com>
2024-12-26 18:10:15 +0800

    feat: 添加一个点击不超过10次的功能

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -3,6 +3,14 @@
   const increment = () => {
     count += 1
   }
+  // 点击不能超过10次否则给出提示
+  $effect(()=>{
+    if (count>10) {
+    console.log(count)
+    alert('不能超过10次')
+    count = 0
+    }  
+  })
 </script>
 
 <button onclick={increment}>
