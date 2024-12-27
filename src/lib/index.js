a474455cc983e430dca23382fb56322d84006498
李龙 <lilong2576@hanweb.com>
2024-12-27 09:43:27 +0800

    feat: 打包组件为webComponent

---
+++ src/lib/index.js
@@ -0,0 +1,5 @@
+import Counter from './Counter.svelte';
+
+export {
+  Counter
+}
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-
+import { resolve } from "path"
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte({
+    compilerOptions: {
+      customElement: true,
+    }})],
+    build: {
+      lib: {
+        entry: resolve(__dirname, 'src/lib/index.js'),
+        name: 'custom-counter',
+        fileName: (fmt) => `build.${fmt}.js`
+      }
+    },
 })
