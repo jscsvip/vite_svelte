b06057566230c81eeeed2857af24d715f24b2139
李龙 <lilong2576@hanweb.com>
2024-12-27 11:16:41 +0800

    feat: if else 对input组件的判断

--- src/lib/Input.svelte
+++ src/lib/Input.svelte
@@ -9,3 +9,11 @@
 </script>
 <!-- 双向绑定 -->
 <input type="text" bind:value>
+
+{#if value > 10}
+  <div>{value}大于10</div>
+{:else if value < 5}
+  <div>{value}小于5</div>
+{:else}
+  <div>{value}大于5且小于10</div>
+{/if}
